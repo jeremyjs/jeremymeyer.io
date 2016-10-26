d2f626d40e51d3655ac6b48638e16422ede797bc
Jeremy Meyer <jeremythedev@gmail.com>
2016-10-26 16:30:27 -0400

    chore: add deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ssh -T appuser@192.81.216.138 'bash -s' << EOF
+  export PATH="$PATH:/home/appuser/.nvm/versions/node/v6.9.1/bin:/home/appuser/.nvm/versions/node/v6.9.1/lib"
+  cd ~/apps/jeremymeyer.io
+  git remote update -p
+  git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
+  pm2 restart all
+EOF
