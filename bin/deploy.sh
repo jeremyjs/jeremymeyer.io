c519942ae35d233ac2b054aeb69de44a5a94fb3d
Jeremy Meyer <jeremythedev@gmail.com>
2016-12-05 17:48:59 -0800

    admin: `git push --tags` before deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git push --tags
+
 ssh -T appuser@192.81.216.138 'bash -s' << EOF
   export PATH="$PATH:/home/appuser/.nvm/versions/node/v6.9.1/bin:/home/appuser/.nvm/versions/node/v6.9.1/lib"
   cd ~/apps/jeremymeyer.io
