8ceb10360e8f885251e152ed4e70199d3aa69967
Jeremy Meyer <jeremythedev@gmail.com>
2017-01-17 00:08:58 -0800

    admin: update deploy script to fetch tags

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,6 +6,7 @@ ssh -T appuser@192.81.216.138 'bash -s' << EOF
   export PATH="$PATH:/home/appuser/.nvm/versions/node/v6.9.1/bin:/home/appuser/.nvm/versions/node/v6.9.1/lib"
   cd ~/apps/jeremymeyer.io
   git remote update -p
+  git fetch --tags
   git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
   pm2 restart jeremymeyer.io
 EOF
